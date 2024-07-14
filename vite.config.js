b3c8912cefb9d11ea58c0c23149ab9229d57680e
이수민 <sooper1137@gmail.com>
2024-07-14 15:41:48 +0900

    헤더 import 셋팅

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,41 @@ import { defineConfig } from "vite";
 import path from "path";
 import { createHtmlPlugin } from "vite-plugin-html";
 import tsconfigPaths from "vite-tsconfig-paths";
+import fs from "fs";
 import requireTransform from "vite-plugin-require-transform";
+
+const headerContent = fs.readFileSync(
+  path.resolve(__dirname, "src/components/layouts/header.html"),
+  "utf-8"
+);
 
 export default defineConfig({
   base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "/components"),
-      "@script": path.resolve(__dirname, "/js"),
-      "@styles": path.resolve(__dirname, "/styles"),
-      "@images": path.resolve(__dirname, "/assets/images"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@script": path.resolve(__dirname, "./src/js"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@images": path.resolve(__dirname, "./src/assets/images"),
       "~": path.resolve(__dirname, "./"),
     },
   },
-  plugins: [createHtmlPlugin(), tsconfigPaths(), requireTransform({})],
+  plugins: [
+    createHtmlPlugin({
+      inject: {
+        data: {
+          header: headerContent,
+        },
+      },
+    }),
+    tsconfigPaths(),
+    requireTransform(),
+  ],
   build: {
+    rollupOptions: {
+      external: ["bootstrap/dist/js/bootstrap.bundle.min.js"],
+    },
     target: "esnext",
     outDir: "dist",
     assetsDir: "./",
