f7c8d6b06e87b9bd8d44fa563eca2bed7e1961d7
이수민 <sooper1137@gmail.com>
2024-07-15 15:16:23 +0900

    헤더 서브메뉴 동작 수정

--- src/js/common.js
+++ src/js/common.js
@@ -1,35 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var menuItems = document.querySelectorAll(".gnb > li > a");
-
-  menuItems.forEach(function (menuItem) {
-    menuItem.addEventListener("mouseover", function () {
-      var parentLi = menuItem.parentNode;
-      var subMenu = parentLi.querySelector(".gnb.sub");
-
+    var menuItems = document.querySelectorAll(".gnb > li");
+  
+    menuItems.forEach(function (menuItem) {
+      var anchor = menuItem.querySelector("a");
+      var subMenu = menuItem.querySelector(".gnb.sub-menu");
+  
+      anchor.addEventListener("mouseover", function () {
+        var allSubMenus = document.querySelectorAll(".gnb.sub-menu");
+        allSubMenus.forEach(function (menu) {
+          menu.classList.remove("on");
+        });
+  
+        if (subMenu) {
+          subMenu.classList.add("on");
+        }
+      });
+  
       if (subMenu) {
-        subMenu.classList.add("on");
+        subMenu.addEventListener("mouseover", function () {
+          var allSubMenus = document.querySelectorAll(".gnb.sub-menu");
+          allSubMenus.forEach(function (menu) {
+            if (menu !== subMenu) {
+              menu.classList.remove("on");
+            }
+          });
+  
+          subMenu.classList.add("on");
+        });
+  
+        subMenu.addEventListener("mouseleave", function () {
+          subMenu.classList.remove("on");
+        });
       }
     });
   });
-
-  var gnb = document.querySelector(".gnb");
-  gnb.addEventListener("mouseleave", function () {
-    var subMenus = gnb.querySelectorAll(".gnb.sub");
-    subMenus.forEach(function (subMenu) {
-      subMenu.classList.remove("on");
-    });
-  });
-
-  var subMenus = document.querySelectorAll(".gnb.sub");
-  subMenus.forEach(function (subMenu) {
-    subMenu.addEventListener("mouseover", function () {
-      subMenu.classList.add("on");
-    });
-  });
-
-  subMenus.forEach(function (subMenu) {
-    subMenu.addEventListener("mouseleave", function () {
-      subMenu.classList.remove("on");
-    });
-  });
-});
+  
