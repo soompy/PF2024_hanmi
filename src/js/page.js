9497147de896514c4f05d55beab1c5c053777348
이수민 <sooper1137@gmail.com>
2024-07-16 15:08:36 +0900

    메인 - 슬라이더 추가

---
+++ src/js/page.js
@@ -0,0 +1,12 @@
+
+
+// import { gsap } from 'gsap';
+
+// const box = document.createElement('div');
+// box.style.width = '100px';
+// box.style.height = '100px';
+// box.style.backgroundColor = 'red';
+// document.getElementById('app').appendChild(box);
+
+// // GSAP을 사용한 애니메이션
+// gsap.to(box, { duration: 2, x: 200, rotation: 360 });
